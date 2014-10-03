4cb50e46fff243e93a370ef92e4bbfc943e530b4
ganye <ganyekins@gmail.com>
2014-10-02 19:46:40 -0700

    working on getting a victim from a list of all currently active users on the wireless network

--- wlans.py
+++ wlans.py
@@ -121,12 +121,52 @@ class Network(object):
         mask = str(len(binary_str.rstrip('0')))
         return '{addr}/{mask}'.format(addr=addr, mask=mask)
 
-class WLANs(object):
-    def __init__(self, interface, nmap=False, msbt=False):
-        self.interface = interface
+class active_users(object):
+    users = []
+    start_time = time.time()
+    current_time = 0
+    mon_mode = False
 
-    def setup(self):
-        self.gateway = get_default_gateway(iface)
+    def packet_callback(self, packet):
+        if packet.hasLayer(Dot11):
+            dot11packet = packet[Dot11]
+            # This block is kind of cryptic -- basically, if the packet is a
+            # Dot11 (Wireless) packet, check to see if any of the IP addresses
+            # present are in our list of users, and if so, increment their 
+            # packet count by one. TODO: Rewrite this in a more straightforward
+            # way
+            if dot11packet.type == 2:
+                addresses = [dot11packet.addr1.upper(),
+                        dot11packet.addr2.upper(),
+                        dot11packet.addr3.upper()]
+                for address in addresses:
+                    for user in self.users:
+                        if address in user[1]:
+                            user[2] += 1
+                self.current_time = time.time()
+
+            if self.current_time > self.start_time + 1:
+                # First, sort users list by data packet count
+                self.users.sort(key=lambda x: float(x[2]), reverse=True)
+                
+                os.system('clear')
+
+                print('[*] {0}IP Address {1}and {2}data {1}sent/received'
+                        .format(Color.tan, Color.white, Color.red))
+    
+
+class WLANs(object):
+    def __init__(self, interface, options):
+        self.network = Network(interface)
+
+        # We want to convert options from an argparse.Namespace object to a
+        # dict for ease of use -- var() does just that for us
+        self.options = var(options)
+
+        self.victim = self.options.get('victim') or active_users.
+
+
+
 
 def main():
     # Check if the user is running as root -- if not, exit
